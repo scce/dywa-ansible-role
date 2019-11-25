d6628426e2b7ae97140e021dec604eb001888fe8
Tim Tegeler <tim.tegeler@tu-dortmund.de>
2019-11-25 10:44:34 +0100

    Remove migrate command and improve build, deploy and restart commands

--- templates/app/app.py
+++ templates/app/app.py
@@ -8,14 +8,14 @@ class Runner(object):
     """
     Class for execute the actual commands
     """
-    docker_path = '{{ bin_docker }}'
-    sudo_path = '{{ bin_sudo }}'
-    deploy_user = '{{ deploy_user }}'
     app_root = '{{ app_root }}'
+    deploy_user = '{{ deploy_user }}'
+    docker_path = '{{ bin_docker }}'
     dywa_database_host = '{{ dywa_database_host }}'
-    dywa_database_user = '{{ dywa_database_user }}'
     dywa_database_password = '{{ dywa_database_password }}'
+    dywa_database_user = '{{ dywa_database_user }}'
     restic_repo_url = '{{ restic_repo_url }}'
+    sudo_path = '{{ bin_sudo }}'
 
     @staticmethod
     def __subprocess(commands):
@@ -42,23 +42,78 @@ class Runner(object):
     def __docker_scale(self, commands):
         self.__docker_service(['scale'] + commands)
 
+    def __build_maintenance_page(self):
+        self.__docker_build(tag='scce/maintenance-page', dockerfile='src/Dockerfile-maintenance-page')
+
+    def __update_service(self, service):
+        self.__docker_service_update(['--force', service])
+
     def __stop_dywa_app(self):
         self.__docker_scale(['app_dywa-app=0'])
 
     def __start_dywa_app(self):
         self.__docker_scale(['app_dywa-app=1'])
 
+    def __start_webapp(self):
+        self.__docker_scale(['app_webapp=1'])
+
+    def __start_nginx(self):
+        self.__docker_scale(['app_nginx=1'])
+
+    def __maintenance_mode_off(self):
+        self.maintenance(mode='off')
+
+    def __maintenance_mode_on(self):
+        self.maintenance(mode='on')
+
+    def __update_dywa_app_and_webapp(self):
+        self.__update_service('app_dywa-app')
+        self.__update_service('app_webapp')
+
+    def __migrate(self, native):
+        self.__docker(
+            [
+                'run',
+                '--rm',
+                '--network=app_postgres',
+                '-v=%s/maven/:/root/.m2' % self.app_root,
+                '-v=%s/src/dywa-app:/usr/src/mymaven' % self.app_root,
+                '-w=/usr/src/mymaven/app-dywa-bridge',
+                'maven:3.5.4',
+                'mvn',
+                '-U',
+                'package',
+                '-Dde.ls5.dywa.jdbc-connection-url=jdbc:postgresql://%s:5432/dywa' % self.dywa_database_host,
+                '-Dde.ls5.dywa.jdbc-user=%s' % self.dywa_database_user,
+                '-Dde.ls5.dywa.jdbc-password=%s' % self.dywa_database_password,
+                '-Dhibernate.dialect=org.hibernate.dialect.PostgreSQLDialect',
+                '-Ddime.native=%s' % ('%s' % native).lower()
+            ]
+        )
+        # the migration container writes as root, so we have to fix the ownerships
+        self.__subprocess(
+            [
+                self.sudo_path,
+                'chown',
+                '-R',
+                '%s:%s' % (self.deploy_user, self.deploy_user),
+                "%s/maven/repository" % self.app_root
+            ]
+        )
+
     def init(self):
         self.__docker_stack(['deploy', '--compose-file', 'docker-compose.yml', 'app'])
+        # build maintenance page so that we can use the maintenance page during initial deployment
+        self.__build_maintenance_page()
 
     def build(self):
         self.__docker(['pull', 'scce/dywa:latest'])
         self.__docker_build(tag='scce/webapp', dockerfile='src/Dockerfile-webapp')
         self.__docker_build(tag='scce/dywa-app', dockerfile='src/Dockerfile-dywa-app')
-        self.__docker_build(tag='scce/maintenance-page', dockerfile='src/Dockerfile-maintenance-page')
+        self.__build_maintenance_page()
 
     def backup(self, command):
-        self.maintenance(mode='on')
+        self.__maintenance_mode_on()
         self.__docker(
             [
                 'run',
@@ -79,51 +134,26 @@ class Runner(object):
                 '--%s' % command
             ]
         )
-        self.maintenance(mode='off')
-
-    def migrate(self, native):
-        self.__stop_dywa_app()
-        self.__docker(
-            [
-                'run',
-                '--rm',
-                '--network=app_postgres',
-                '-v=%s/maven/:/root/.m2' % self.app_root,
-                '-v=%s/src/dywa-app:/usr/src/mymaven' % self.app_root,
-                '-w=/usr/src/mymaven/app-dywa-bridge',
-                'maven:3.5.4',
-                'mvn',
-                '-U',
-                'package',
-                '-Dde.ls5.dywa.jdbc-connection-url=jdbc:postgresql://%s:5432/dywa' % self.dywa_database_host,
-                '-Dde.ls5.dywa.jdbc-user=%s' % self.dywa_database_user,
-                '-Dde.ls5.dywa.jdbc-password=%s' % self.dywa_database_password,
-                '-Dhibernate.dialect=org.hibernate.dialect.PostgreSQLDialect',
-                '-Ddime.native=%s' % ('%s' % native).lower()
-            ]
-        )
-        self.__subprocess(
-            [
-                self.sudo_path,
-                'chown',
-                '-R',
-                '%s:%s' % (self.deploy_user, self.deploy_user),
-                "%s/maven/repository" % self.app_root
-            ]
-        )
+        self.__maintenance_mode_off()
 
     def deploy(self, native):
-        self.migrate(native)
+        self.__maintenance_mode_on()
+        self.__migrate(native)
         self.build()
-        self.restart()
+        self.__update_dywa_app_and_webapp()
+        self.__maintenance_mode_off()
+        # for the initial deployment we have to make sure that nginx is started
+        self.__start_nginx()
 
     def restart(self):
-        services = ['app_webapp', 'app_dywa-app', 'app_nginx']
-        for service in services:
-            self.__docker_scale(['%s=1' % service])
-            self.__docker_service_update(['--force', service])
+        self.__update_dywa_app_and_webapp()
+        self.__start_dywa_app()
+        self.__start_webapp()
+        self.__start_nginx()
 
     def maintenance(self, mode):
+        # for the initial deployment we have to make sure that webapp is started
+        self.__start_webapp()
         if mode == 'off':
             self.__start_dywa_app()
             self.__docker_service_update_image(['scce/webapp', 'app_webapp'])
@@ -153,7 +183,6 @@ class App(object):
         parser = argparse.ArgumentParser(description='Script to control the app deployment')
         subparsers = self.create_subparsers(parser)
         self.create_init_parser(subparsers)
-        self.create_migrate_parser(subparsers)
         self.create_build_parser(subparsers)
         self.create_backup_parser(subparsers)
         self.create_deploy_parser(subparsers)
@@ -176,18 +205,6 @@ class App(object):
             help='Initialize the app stack before you start deploying'
         )
         init_parser.set_defaults(func=self.init)
-
-    def create_migrate_parser(self, subparsers):
-        migrate_parser = subparsers.add_parser(
-            name='migrate',
-            help='Migrate dywa database schema',
-        )
-        migrate_parser.add_argument(
-            '--native',
-            action='store_true',
-            help='Use native database schema'
-        )
-        migrate_parser.set_defaults(func=self.migrate)
 
     def create_build_parser(self, subparsers):
         build_parser = subparsers.add_parser(
@@ -259,12 +276,6 @@ class App(object):
         Initialize the app stack before you start deploying
         """
         self.runner.init()
-
-    def migrate(self):
-        """
-        Migrate dywa database schema
-        """
-        self.runner.migrate(self.args.native)
 
     def build(self):
         """
